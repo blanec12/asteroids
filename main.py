a2088f3bd5b38fdbd66ebef3b3eeece74992d21d
Blane Cummings <blanecummings@hotmail.com>
2025-12-15 11:12:56 -0600

    render asteroids on screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from player import Player
 
 
@@ -10,10 +12,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -23,13 +35,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) / 1000
-        print(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
